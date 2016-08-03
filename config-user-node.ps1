62fd9a51e2423d28a3f9355ad2b6f59fd9e942da
Gordon Watts <gordon@gordonandpaula.net>
2016-08-03 19:04:46 -0400

    Initial Import

---
+++ config-user-node.ps1
@@ -0,0 +1,23 @@
+#
+# Run to configure a node with the tools we often use to do work.
+#
+
+# See if the package feed is there. If not, then install it.
+$repo = Get-PSRepository | ? {$_.Name -eq "atlas-myget"}
+if (!$repo) {
+	Register-PSRepository -name "atlas-myget" -source https://www.myget.org/F/gwatts-powershell/api/v2 -InstallationPolicy Trusted
+}
+
+# Next, if we don't have a module, then update it.
+function install-or-update ($module)
+{
+	$listing = get-module -ListAvailable $module
+	if ($listing) {
+		Update-Module $module
+	} else {
+		Install-Module -scope CurrentUser $module
+	}
+}
+
+install-or-update "PSJenkinsAccessCommands"
+install-or-update "PSAtlasDatasetCommands"
